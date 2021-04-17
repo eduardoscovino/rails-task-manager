68a9527ab570ce1dbccd695fb982fa010a9649e8
Eduardo Scovino <eduardo.scovino@gmail.com>
2021-04-17 16:16:05 -0300

    update the task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,9 +8,23 @@ class TasksController < ApplicationController
     @task = Task.new
   end
 
+  def show
+    @task = Task.find(params[:id])
+  end
+
   def create
     @task = Task.new(task_params)
     @task.save
+    redirect_to tasks_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
     redirect_to tasks_path(@task)
   end
 
